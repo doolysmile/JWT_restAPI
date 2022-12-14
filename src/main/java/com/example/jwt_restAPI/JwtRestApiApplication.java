46c252999cc2cea85b8c13516a0ab5f3d28e3cce
KCH <doolysmile1@naver.com>
2022-12-14 16:38:53 +0900

    chore : 기초 환경 세팅

--- src/main/java/com/example/jwt_restAPI/JwtRestApiApplication.java
+++ src/main/java/com/example/jwt_restAPI/JwtRestApiApplication.java
@@ -2,12 +2,20 @@ package com.example.jwt_restAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class JwtRestApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JwtRestApiApplication.class, args);
 	}
-
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
---
+++ src/main/java/com/example/jwt_restAPI/app/member/service/MemberService.java
@@ -0,0 +1,24 @@
+package com.example.jwt_restAPI.app.member.service;
+
+import com.example.jwt_restAPI.app.member.entity.Member;
+import com.example.jwt_restAPI.app.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public Member join(String username, String password, String email) {
+        Member member = Member.builder()
+                .username(username)
+                .password(password)
+                .email(email)
+                .build();
+
+        memberRepository.save(member);
+
+        return member;
+    }
+}
