c966a772c41962ddb2013483a7e04459bfef716a
KCH <doolysmile1@naver.com>
2022-12-14 16:13:23 +0900

    first commit

---
+++ src/main/java/com/example/jwt_restAPI/JwtRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwt_restAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtRestApiApplication.class, args);
+	}
+
+}
